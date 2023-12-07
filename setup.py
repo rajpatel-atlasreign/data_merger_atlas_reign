f9a7936cff6b9326ec12d32d8283ca2cadb76998
RAJ PATEL <rajpatel@atlasreign.com>
2023-12-07 15:30:32 +0530

    [fix] package setup update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='data_merger',
-    version='0.1',
+    version='0.0',
     packages=find_packages(),
     install_requires=[
         'pymysql',
