0a404714593cbdf1c58a7ec439e4859faf04398d
RAJ PATEL <rajpatel@atlasreign.com>
2023-12-07 16:05:15 +0530

    [feat] Package name update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     # Basic package information
-    name='data_merger',
+    name='atlasreign_datamanager',
     version='0.0.0',
     packages=find_packages(),
 
