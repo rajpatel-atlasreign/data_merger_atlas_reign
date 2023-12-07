ccbb45da87bf4de7ff446eaa88696b248e7f9ab0
RAJ PATEL <rajpatel@atlasreign.com>
2023-12-07 15:37:12 +0530

    [fix] package setup update

--- setup.py
+++ setup.py
@@ -1,17 +1,46 @@
 from setuptools import setup, find_packages
 
 setup(
+    # Basic package information
     name='data_merger',
-    version='0.0',
+    version='0.1.0',
     packages=find_packages(),
+
+    # Metadata
+    author='RAJ PATEL',
+    author_email='rajpatel@atlasreign.com',
+    description='Data Merger: A Python package for merging data from two tables in a MySQL database. The service '
+                'reads a configuration file that specifies the details of the merge operation, such as the names of '
+                'the tables, the keys to join on, the type of join, and the output file name. Easy to use and highly '
+                'flexible, Data Merger simplifies the process of merging data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/data_merger',
+
+    # Dependencies
     install_requires=[
         'pymysql',
         'sqlalchemy',
         'pandas'
     ],
+
+    # Entry points
     entry_points={
         'console_scripts': [
             'data_merge_service = data_merge_service.main:main',
         ],
     },
+
+    # Classifiers
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+
+    # Python version requirement
+    python_requires='==3.10.*',  # Specific to Python 3.10
 )
